79f99b6d59b59e4b4f093c2286436337c121ca50
flwwsg <flwwsg@qq.com>
2021-06-05 10:59:26 +0800

    add script

---
+++ assets/scripts/player.js
@@ -0,0 +1,30 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // 生成的地板范围
+        landRange: cc.v2(20, 300),
+        hero: cc.Node,
+        firstLand: cc.Node,
+
+        canvas: cc.Node,
+        scoreLabel: cc.Label,
+        highestScoreLabel: cc.Label,
+    },
+
+    onLoad () {
+        // 棍子
+        this.stick = null;
+        // 棍子是否在按压（伸长）
+        this.isPress = false;
+        this.score = 0;
+        this.currentLandRange = 0;
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
